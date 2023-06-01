a6b5d8ef95253c30635f206bff6e92f30f58425b
Mariia Bozhenko <56414957+m-bojenko@users.noreply.github.com>
2023-06-01 23:57:08 +0300

    Update database_work.py

--- programming/udz2/database_work.py
+++ programming/udz2/database_work.py
@@ -51,9 +51,18 @@ class DataBase:
                 return trans
 
     def show_sort_transport(self):
+        """ Отсортированный по грузоподъемности список машин """
+
         trans = self.show_transport()
         trans.sort(key = lambda x: x[5])
         return trans
+
+    def show_free_transport(self, date):
+        set1 = set(self.show_sort_transport())
+        set2 = set(self.show_booked_transport(date))
+        result_set = set1 - set2
+        result = list(result_set)
+        return result
 
     def show_booked_transport(self, date):
         try:
@@ -70,7 +79,7 @@ class DataBase:
 
             cars = []
             for i in self.show_transport():
-                if i in id_cars:
+                if i[0] in id_cars:
                     cars.append(i)
 
             connection.commit()
@@ -84,6 +93,65 @@ class DataBase:
                 connection.close()
                 print("Соединение с SQLite закрыто")
                 return cars
+
+    def show_booked_drivers(self, date):
+        try:
+            connection = sqlite3.connect('transportation.db')
+
+            cursor = connection.cursor()
+            print("База данных успешно подключена к SQLite")
+
+            cursor.execute('''SELECT * FROM tApplications WHERE date = ?''', (date,))
+            apps = cursor.fetchall()
+            id_drivers = []
+            for i in apps:
+                id_drivers.append(i[10])
+
+            drivers = []
+            for i in self.show_driver():
+                if i in id_drivers:
+                    drivers.append(i)
+
+            connection.commit()
+
+            cursor.close()
+        except sqlite3.Error as error:
+            print("Ошибка при подключении к sqlite", error)
+        finally:
+            if (connection):
+                connection.commit()
+                connection.close()
+                print("Соединение с SQLite закрыто")
+                return drivers
+
+    def show_free_drivers(self, date):
+        set1 = set(self.show_driver())
+        set2 = set(self.show_booked_drivers(date))
+        result_set = set1 - set2
+        result = list(result_set)
+        return result
+
+    def show_driver(self):
+        try:
+            connection = sqlite3.connect('transportation.db')
+
+            cursor = connection.cursor()
+            print("База данных успешно подключена к SQLite")
+
+            cursor.execute('''SELECT * FROM tDrivers''')
+            dris = cursor.fetchall()
+
+            connection.commit()
+
+            cursor.close()
+        except sqlite3.Error as error:
+            print("Ошибка при подключении к sqlite", error)
+        finally:
+            if (connection):
+                connection.commit()
+                connection.close()
+                print("Соединение с SQLite закрыто")
+                return dris
 
     def add_application(self, customer_name, contact_name, contacts, cargo_tonnage, cargo_length, cargo_width,
                         cargo_height, date, idTransport, idDriver):
@@ -129,3 +197,13 @@ class DataBase:
                 connection.close()
                 print("Соединение с SQLite закрыто")
 
+    def show_suitable_transport(self, weight, length, width, height, date):
+        trans = self.show_free_transport(date)
+        suit_trans = []
+        for i in trans:
+            if i[5] >= weight and i[6] > length and i[7] > width and i[8] > height:
+                suit_trans.append(i)
+        return suit_trans
+
+
+#  print(DataBase().show_booked_transport('23.05.2023'))
